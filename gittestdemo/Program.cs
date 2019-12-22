19d0a5717111adf9dd50f87e145c6472f8f19dc5
Nicolaenko Nikita <n.shevcov906@gmail.com>
2019-12-22 19:13:13 +0200

    All files in place

---
+++ gittestdemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gittestdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
